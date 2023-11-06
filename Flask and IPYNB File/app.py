c7d09c714bf2587f1f9b2c285f3935ec808c9571
Anjali Srivastava <anjalisri1810@example.com>
2023-11-06 17:52:18 +0530

    first commit

--- Flask and IPYNB File/app.py
+++ Flask and IPYNB File/app.py
@@ -1,39 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[21]:
-
-
 from flask import Flask,render_template,request
-
-
-# In[22]:
-
-
 app=Flask(__name__)
-
-
-# In[23]:
-
-
 import pickle
-
-
-# In[24]:
-
-
 import numpy as np
-
-
-# In[25]:
-
-
 model= pickle.load(open('occupancy.pkl','rb'))
-
-
-# In[26]:
-
-
 @app.route('/')
 def start():
     return render_template("index.html")
@@ -51,20 +20,9 @@ def login():
     res=[[float(t),float(h),float(l),float(c),float(hum),float(ye),float(m),float(d)]]
     out=model.predict(res)
     print(out)
-    return render_template("index.html", y="The Occupancy Profit is "+str((out[0])))
+    return render_template("index.html", y="The Occupancy Rate is "+str((out[0])))
 if __name__== '__main__':
     app.run(debug=False,host='0.0.0.0')
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
